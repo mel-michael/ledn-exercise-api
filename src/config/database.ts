27fa8b870d67438f8dee3da8d9ce9cd28860e845
Mel Michael <mel.michael007@gmail.com>
2021-08-21 18:28:53 -0700

    Add connection to mongo db

---
+++ src/config/database.ts
@@ -0,0 +1,8 @@
+export const dbConfig = {
+  dbName: process.env.DB_NAME,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  collectionName: process.env.COLLECTION_NAME,
+  host: process.env.DB_HOST
+};
+
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,44 @@
+import * as dotenv from 'dotenv';
 import express, { Request, Response, Application } from 'express';
+import { MongoClient, Collection, Db } from 'mongodb';
+
+dotenv.config();
+
+import { dbConfig } from './config/database';
+const { host, dbName = 'mydb', collectionName = 'accounts' } = dbConfig;
+const uri = `mongodb://${host}?writeConcern=majority`;
+
+export const collections: { accounts?: Collection } = {};
+
+export const connectToDatabase = async function () {
+  const client: MongoClient = new MongoClient(uri);
+
+  await client.connect();
+
+  const db: Db = client.db(dbName);
+
+  const accountCollection: Collection = db.collection(collectionName);
+
+  collections.accounts = accountCollection;
+
+  console.log(
+    `Successfully connected to database: ${db.databaseName} & collection: ${accountCollection.collectionName}`
+  );
+};
+
+// Connect to database
+connectToDatabase();
 
 const app: Application = express();
-
 const PORT = process.env.PORT || 7000;
 
-app.get('/', (req: Request, res: Response): void => {
-  res.send('Hello Typescript with Node.js!');
+app.get('/', async (req: Request, res: Response): Promise<void> => {
+  try {
+    const holders = await collections.accounts?.find({}).toArray();
+    res.status(200).send(holders);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 app.listen(PORT, (): void => {
